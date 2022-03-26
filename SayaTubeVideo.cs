1ddfb3a5adb54bdbcbe52862adf246e4feab37e8
RevDonz <aprilio842@gmail.com>
2022-03-26 10:27:35 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal5_1302204051
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rnd = new Random();
+            this.id = rnd.Next(0, 100000);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int data)
+        {
+            Debug.Assert(this.playCount <= int.MaxValue);
+            Debug.Assert(data <= 10000000, "Input tidak boleh lebih dari 10 juta");
+
+
+            int test;
+            try
+            {
+                test = checked(this.playCount + data);
+                this.playCount += data;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+                this.playCount = this.playCount;
+            }
+
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Id : " + this.id);
+            Console.WriteLine("Title : " + this.title);
+            Console.WriteLine("Play Count : " + this.playCount + "\n");
+        }
+
+        public int getPlayCount()
+        {
+            return this.playCount;
+        }
+
+        public string getTitle()
+        {
+            return this.title;
+        }
+    }
+}
